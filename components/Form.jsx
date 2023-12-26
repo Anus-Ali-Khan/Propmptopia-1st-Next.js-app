f90f91b7367786bd07f1b4c39b67deaf563177d2
Anus Ali Khan <anusalikhan1405@gmail.com>
2023-12-26 13:03:12 +0500

    Feed page is completed displaying all th prompts created by user

--- components/Form.jsx
+++ components/Form.jsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
   return (
-    <section className="w-full max-w-full flex justify-start flex-col">
+    <section className="w-full max-w-full flex-start flex-col">
       <h1 className="head_text text-left">
         <span className="blue_gradient">{type} Post</span>
       </h1>
@@ -41,7 +41,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             className="form_input"
           />
         </label>
-        <div className="flex justify-end mx-3 mb-5 gap-4">
+        <div className="flex-end mx-3 mb-5 gap-4">
           <Link href="/" className="text-gray-500 px-5 py-1.5 text-sm">
             Cancel
           </Link>
